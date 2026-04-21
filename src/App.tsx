87365f4cac54b8750bf7f43e2cdc28cb90286d4b
HumbleG0d <humbleg0d7642@gmail.com>
2026-04-21 16:43:59 -0500

    CHANGE: App.tsx and add Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import { Home } from './pages/Home';
+
 function App() {
-  return (
-    <div className="min-h-screen bg-black">
-      <h1 className="text-3xl font-bold text-center py-10">Infinitia</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
