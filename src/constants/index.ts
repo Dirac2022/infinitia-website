e65d4b20c9b491e2d9caf64f93fc647638cccef5
HumbleG0d <humbleg0d7642@gmail.com>
2026-04-21 17:25:42 -0500

    CHANGE: separation components

--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const NAV_LINKS = [
-  { label: 'Servicios',  href: '#services' },
-  { label: 'Proceso',    href: '#process' },
-  { label: 'Equipo',    href: '#team' },
-  { label: 'FAQ',       href: '#faq' },
-  { label: 'Contacto',     href: '#contact' }
-];
