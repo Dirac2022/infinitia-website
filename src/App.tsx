c54e8e79e48f6553367288a7fefed0b9449901e6
HumbleG0d <humbleg0d7642@gmail.com>
2026-04-21 15:46:27 -0500

    ADD: initial projecto configuration

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black">
+      <h1 className="text-3xl font-bold text-center py-10">Infinitia</h1>
+    </div>
+  );
+}
+
+export default App;
