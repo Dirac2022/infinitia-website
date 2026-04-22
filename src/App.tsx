1c423bf911859af93965318b89a70651b0eaa076
Beardynh <jhontimoteogarcia@gmail.com>
2026-04-22 17:37:15 -0500

    feat: agregado lenis para efecto de scroll fisico

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
+
 import { LangProvider } from './context/LangContext';
 import { Home } from './pages/Home';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+      lerp: 0.08, // Efecto de inercia y suavidad perfecto
+    });
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <LangProvider>
       <Home />
